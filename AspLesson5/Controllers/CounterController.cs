687bc3131af560c04dd90d29657f656641737446
korolka <korolka212@gmail.com>
2023-10-03 13:13:10 +0300

    first commit

---
+++ AspLesson5/Controllers/CounterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspLesson5.Controllers
+{
+    public class CounterController : Controller
+    {
+        public static string key = "guidCodeUser";
+        //public static int counter;
+        public static List<Guid> guids;
+
+        static CounterController()
+        {
+            guids = new List<Guid>();
+        }
+        public IActionResult Count()
+        {
+            if(guids.Count==0)
+            return View(0);
+            
+            return View(guids.Count);
+        }
+        [HttpPost]
+        public IActionResult Increase()
+        {
+            string userGuid = HttpContext.Session.GetString(key);
+            if (string.IsNullOrEmpty(userGuid))
+            {
+                Guid guid = Guid.NewGuid();
+                guids.Add(guid);
+                userGuid = guid.ToString();
+                HttpContext.Session.SetString(key, userGuid);
+            }
+
+            return Ok(guids.Count);
+        }
+        [HttpPost]
+        public IActionResult Decrease()
+        {
+            Guid.TryParse((HttpContext.Session.GetString(key)),out Guid userGuid );
+            guids.Remove(userGuid);
+            return Ok(guids.Count);
+        }
+    }
+
+}
---
+++ AspLesson5/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspLesson5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string key = "def";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string value, DateTime date)
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = date;
+            Response.Cookies.Append(key, value, options);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult GetValue()
+        {
+            string value = Request.Cookies[key];
+            return View(value as object);
+        }
+    }
+}
